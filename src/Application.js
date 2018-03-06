9a67125ebd6a435ec2d7fa0d59ac0cb2d993bd24
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-03-06 14:17:42 +0100

    oauth implementd

--- src/Application.js
+++ src/Application.js
@@ -9,14 +9,27 @@ import './Application.css';
 class Application extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((currentUser) => {
+      this.setState({ currentUser })
+    })
   }
 
   render() {
+    const  { currentUser} = this.state
     return (
       <div className="Application">
         <header className="Application--header">
           <h1>Lunch Rush</h1>
         </header>
+        <div>
+          {!currentUser ? <SignIn /> : <CurrentUser user={currentUser} />}
+        </div>
       </div>
     );
   }
